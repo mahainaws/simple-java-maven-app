62c904842fc88b32cfc7d63a26a0e268321232af
mahainaws <69946513+mahainaws@users.noreply.github.com>
2025-03-08 11:55:57 -0500

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -5,9 +5,9 @@ set -x
 mvn jar:jar install:install
 set +x
 
-# Extract project name and version safely (removes color codes)
-NAME=$(mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout | tr -d '\r')
-VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout | tr -d '\r')
+# Extract project name and version safely (removes extra output)
+NAME=$(mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout | tr -d '\r' | tr -d '[:space:]')
+VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout | tr -d '\r' | tr -d '[:space:]')
 
 echo "Project Name: $NAME"
 echo "Project Version: $VERSION"
